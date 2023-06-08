4062ecc3293dfa2d779abd4639ed42ecc9f1dca0
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-06-08 10:38:21 +0300

    feat: add logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './index.scss';
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={decreaseCount}>
+          - Минус
+        </button>
+        <button className="plus" onClick={increaseCount}>
+          Плюс +
+        </button>
       </div>
     </div>
   );
