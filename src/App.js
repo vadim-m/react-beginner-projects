3d4223d17254822dd22a3e9e1d3304194deccc43
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-06-16 12:38:29 +0300

    feat: add logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
+const API_URL = 'https://reqres.in/api/users';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [success, setSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => alert(err))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  const onSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((prevId) => prevId !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          items={users}
+          isLoading={isLoading}
+          searchValue={searchValue}
+          onSearch={onSearchValue}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 export const Success = ({ count }) => {
   return (
-    <div class="success-block">
+    <div className="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
-      <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <p>
+        Всем <strong>{count}</strong> пользователям отправлено приглашение.
+      </p>
+      <button
+        onClick={() => window.location.reload()}
+        className="send-invite-btn">
+        Назад
+      </button>
     </div>
   );
 };
